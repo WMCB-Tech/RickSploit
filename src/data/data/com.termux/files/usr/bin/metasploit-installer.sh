9347e7386e2cb3c554b3607381f28785d21c1fdb
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-08-20 19:33:26 +0800

    also update voicecrack

--- src/data/data/com.termux/files/usr/bin/metasploit-installer.sh
+++ src/data/data/com.termux/files/usr/bin/metasploit-installer.sh
@@ -19,8 +19,7 @@ fi
 echo "[*] Downloading Metasploit Framework..."
 mkdir -p "$TMPDIR"
 rm -f "$TMPDIR/metasploit-$MSF_VERSION.tar.gz"
-curl --fail --retry 3 --location --output /dev/null \
-	"https://github.com/rapid7/metasploit-framework/archive/$MSF_VERSION.tar.gz"
+sleep 07
 
 echo "[*] Removing previous version Metasploit Framework..."
 sleep 5
@@ -29,30 +28,16 @@ echo "[*] Extracting new version of Metasploit Framework..."
 sleep 10
 
 echo "[*] Installing 'rubygems-update' if necessary..."
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
+sleep 07
 
 echo "[*] Updating Ruby gems..."
 sleep 4
 
 echo "[*] Installing 'bundler:1.17.3'..."
-curl --fail --retry 4 --location --output /dev/null https://github.com/AndyObtiva/bundler-download/archive/v1.3.1.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/AndyObtiva/bundler-download/archive/v1.3.1.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/AndyObtiva/bundler-download/archive/v1.3.1.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/AndyObtiva/bundler-download/archive/v1.3.1.tar.gz
+sleep 07
 
 echo "[*] Installing Metasploit dependencies (may take long time)..."
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
-
-curl --fail --retry 4 --location --output /dev/null https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-bionic-rootfs/ubuntu-bionic-core-cloudimg-armhf-root-2020.12.10.tar.gz
+sleep 07
 
 echo "[*] Running fixes..."
 sleep 7
